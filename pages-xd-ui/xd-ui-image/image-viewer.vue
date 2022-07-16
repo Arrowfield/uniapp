f1b810c91ebbb17d7fe4e1e8e12002f613976200
Arrowfield <768449566@qq.com>
2022-07-16 10:54:31 +0800

    图片预览

--- pages-xd-ui/xd-ui-image/image-viewer.vue
+++ pages-xd-ui/xd-ui-image/image-viewer.vue
@@ -1,30 +1,77 @@
 <template>
-	<xd-vue-layout  className="bg-black">
+	<xd-vue-layout className="bg-black">
 		<xd-vue-navigation>
 			<view class="left-btn"></view>
-			<text style="color: #fff;">1/10</text>
+			<text style="color: #fff;">{{current + 1}}/{{imageList.length}}</text>
 		</xd-vue-navigation>
-		<!-- <view>this is a image page</view> -->
+
+
+		<view class="uni-margin-wrap">
+			<swiper 	@change="bindchange" class="swiper" 	:current="current" circular :indicator-dots="indicatorDots" :autoplay="false" :interval="interval"
+				:duration="duration">
+				<swiper-item :key="index" v-for="(item,index) of imageList">
+					<image 	 class="image" :src="item" @click="viewerImage"/>
+				</swiper-item>
+				
+			</swiper>
+		</view>
+
 	</xd-vue-layout>
 </template>
 
 <script>
-	export default{
-		data(){
+	export default {
+		data() {
 			return {
-				imageList:[
-					
-				]
+				imageList: [
+					"https://cdn.zipblog.top/switch/switch-pro01.jpg",
+					"https://cdn.zipblog.top/switch/switch-pro02.jpg",
+					"https://cdn.zipblog.top/switch/switch-pro03.jpg",
+					"https://cdn.zipblog.top/switch/switch-pro04.jpg",
+					"https://cdn.zipblog.top/switch/switch-pro05.jpg"
+				],
+				background: ['color1', 'color2', 'color3'],
+				indicatorDots: true,
+				autoplay: true,
+				interval: 2000,
+				duration: 500,
+				current:0
+			}
+		},
+		methods:{
+			bindchange(event){
+				this.current = event.mp.detail.current
+			},
+			viewerImage(){
+				uni.previewImage({
+					urls:this.imageList
+				})
 			}
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
+	.uni-margin-wrap {
+		position: absolute;
+		top:50%;
+		left:50%;
+		width: 100%;
+		transform: translate(-50%,-50%);
+	}
 
+	.swiper {
+		height: 750rpx;
+	}
+
+
+	.image{
+		width: 750rpx;
+		height: 750rpx;
+	}
 </style>
 <style lang="scss">
-	.bg-black{
+	.bg-black {
 		background: #000;
 		color: #fff;
 		height: 100vh;
